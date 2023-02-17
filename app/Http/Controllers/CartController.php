08c5a2b8d9f8228748f73625a52d3c7fd572fed4
donat987 <dalosdoni@icloud.com>
2023-02-17 18:10:35 +0100

    0217

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,8 +10,52 @@ class CartController extends Controller
 {
     public function cart()
     {
-        echo "hello";
+       ?>
+        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Termék neve</th>
+                                    <th>Darabár ár</th>
+                                    <th>Darabszám</th>
+                                    <th>Teljes összeg</th>
+                                    <th>Törlés</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach (json_decode(Cookie::get('cart')) as $sor){?>
+                                    <tr>
+                                        <td><img width="100px" src="<?php echo $sor->file; ?>"</td>
+                                        <td class="align-middle"><?php echo $sor->product_name; ?></td>
+                                        <td class="align-middle"><?php echo$sor->oneprice; ?> Ft</td>
+                                        <td class="align-middle">
+                                            <div class="input-group mb-3" style="max-width: 120px;">
+                                                <div class="input-group-prepend">
+                                                    <button class="btn btn-outline-primary js-btn-minus"
+                                                        id="<?php echo $sor->link; ?>" onclick="kivon(this.id)" name="minus"
+                                                        type="button">−</button>
+                                                </div>
+                                                <input type="text" id="" style="padding: 6px"
+                                                    class="form-control text-center border mr-0"
+                                                    value="<?php echo $sor->quantity ?>" readonly="e<?php echo $sor->link; ?>"
+                                                    id="">
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-outline-primary js-btn-plus"
+                                                        id="<?php echo $sor->link; ?>" onclick="hozzaad(this.id)" name="plus"
+                                                        type="button">+</button>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="align-middle"><?php echo $sor->price; ?> Ft</td>
+                                        <td class="align-middle"><a><i class="fa fa-trash fa-2x" aria-hidden="true"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php }?>
+                            </tbody>
+                        </table>
+       <?php
     }
+
     public function delet()
     {
         Cookie::queue(Cookie::forget('cart'));
--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -9,48 +9,7 @@
 
                     </header><!-- sect-heading -->
                     <div class="row" id="kocsi">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Termék neve</th>
-                                    <th>Darabár ár</th>
-                                    <th>Darabszám</th>
-                                    <th>Teljes összeg</th>
-                                    <th>Törlés</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach (json_decode(Cookie::get('cart')) as $sor)
-                                    <tr>
-                                        <td><img width="100px" src="{{ $sor->file }}"</td>
-                                        <td class="align-middle">{{ $sor->product_name }}</td>
-                                        <td class="align-middle">{{ $sor->oneprice }} Ft</td>
-                                        <td class="align-middle">
-                                            <div class="input-group mb-3" style="max-width: 120px;">
-                                                <div class="input-group-prepend">
-                                                    <button class="btn btn-outline-primary js-btn-minus"
-                                                        id="{{ $sor->link }}" onclick="kivon(this.id)" name="minus"
-                                                        type="button">−</button>
-                                                </div>
-                                                <input type="text" id="" style="padding: 6px"
-                                                    class="form-control text-center border mr-0"
-                                                    value="{{ $sor->quantity }}" readonly="e{{ $sor->link }}"
-                                                    id="">
-                                                <div class="input-group-append">
-                                                    <button class="btn btn-outline-primary js-btn-plus"
-                                                        id="{{ $sor->link }}" onclick="hozzaad(this.id)" name="plus"
-                                                        type="button">+</button>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle">{{ $sor->price }} Ft</td>
-                                        <td class="align-middle"><a><i class="fa fa-trash fa-2x" aria-hidden="true"></i></a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        
                     </div> <!-- row.// -->
                     <script>
                         kosarbetolt();
@@ -61,7 +20,7 @@
                                 url: "{{ route('cartt') }}",
                                 method: "get",
                                 data: {_token: _token},
-                                succes: function(data) {
+                                success: function(data) {
                                     $('#kocsi').html(data);
                                 }
                             });
